1405a0ec3d8ab5e76a1c704c403358ac253d8d51
Henry Arneson <h.arneson@wustl.edu>
2020-04-08 16:48:34 -0500

    e

--- PhotoShare/app/src/main/java/com/example/photoshare/FeedActivity.kt
+++ PhotoShare/app/src/main/java/com/example/photoshare/FeedActivity.kt
@@ -6,15 +6,14 @@ import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
 import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import com.example.photoshare.ui.main.SectionsPagerAdapter
 
-class MainActivity : AppCompatActivity() {
+class FeedActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_feed)
+
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = findViewById(R.id.view_pager)
         viewPager.adapter = sectionsPagerAdapter
