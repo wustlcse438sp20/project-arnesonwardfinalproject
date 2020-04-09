79da77c9910714420afa5e52c88e8defd9dcb8ba
Henry Arneson <h.arneson@wustl.edu>
2020-04-08 21:30:41 -0500

    2 frags

--- PhotoShare/app/src/main/java/com/example/photoshare/ui/main/SectionsPagerAdapter.kt
+++ PhotoShare/app/src/main/java/com/example/photoshare/ui/main/SectionsPagerAdapter.kt
@@ -4,12 +4,9 @@ import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.example.photoshare.AccountFragment
+import com.example.photoshare.FeedFragment
 import com.example.photoshare.R
-
-private val TAB_TITLES = arrayOf(
-    R.string.tab_text_1,
-    R.string.tab_text_2
-)
 
 /**
  * A [FragmentPagerAdapter] that returns a fragment corresponding to
@@ -21,11 +18,19 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
     override fun getItem(position: Int): Fragment {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1)
+        return when (position) {
+            0 -> FeedFragment()
+            1 -> AccountFragment()
+            else -> FeedFragment()
+        }
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
-        return context.resources.getString(TAB_TITLES[position])
+        return when (position) {
+            0 -> "Feed"
+            1 -> "Account"
+            else -> "idk"
+        }
     }
 
     override fun getCount(): Int {
