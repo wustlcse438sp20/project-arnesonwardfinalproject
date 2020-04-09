4bf036f33f705e143a70d41828fbe71556b26de9
Ethan Ward <ehward@wustl.edu>
2020-04-09 17:38:50 -0500

    MANY E RECYCLER VIEWS YES

--- PhotoShare/app/src/main/java/com/example/photoshare/CollectionViewAdapter.kt
+++ PhotoShare/app/src/main/java/com/example/photoshare/CollectionViewAdapter.kt
@@ -1,7 +1,43 @@
 package com.example.photoshare
 
-class CollectionViewAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+
+
+class CollectionViewViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
+    RecyclerView.ViewHolder(inflater.inflate(R.layout.collection_view_item, parent, false)) {
+    private val image: ImageView
+
+
+    init {
+        image = itemView.findViewById(R.id.collectionItem)
+    }
+
+
+    fun bind(url: String, i: Int) {
+        Picasso.get().load(url).into(image)
+    }
+
 }
 
+class CollectionViewAdapter(private val list: ArrayList<String>)
+    : RecyclerView.Adapter<CollectionViewViewHolder>() {
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CollectionViewViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return CollectionViewViewHolder(inflater, parent)
+    }
+
+    override fun onBindViewHolder(holder: CollectionViewViewHolder, position: Int) {
+        val url: String = list[position]
+        holder.bind(url, position)
+    }
+
+    override fun getItemCount(): Int = list.size
+
+}
 
--- PhotoShare/app/src/main/java/com/example/photoshare/ViewCollectionActivity.kt
+++ PhotoShare/app/src/main/java/com/example/photoshare/ViewCollectionActivity.kt
@@ -2,11 +2,24 @@ package com.example.photoshare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_view_collection.*
 
 class ViewCollectionActivity : AppCompatActivity() {
+
+    companion object{
+        val collectionItemList: ArrayList<String> = ArrayList()
+        val url1 = "https://i.kym-cdn.com/entries/icons/mobile/000/026/008/Screen_Shot_2018-04-25_at_12.24.22_PM.jpg"
+        val url2 = "https://i.kym-cdn.com/entries/icons/mobile/000/026/008/Screen_Shot_2018-04-25_at_12.24.22_PM.jpg"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_collection)
+        collectionItemList.add(url1)
+        collectionItemList.add(url2)
+        var collectionviewadapter = CollectionViewAdapter(collectionItemList)
+        recyclerView.adapter = collectionviewadapter
+        recyclerView.layoutManager = LinearLayoutManager(this)//GridLayoutManager(this, 1, GridLayoutManager.VERTICAL, false)
     }
 }
